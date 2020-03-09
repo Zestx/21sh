c00f6e9d44d211750425f387d7b41e3fb3a733ef
quro19 <quro.19.19@gmail.com>
2020-03-09 19:39:53 +0100

    advanced on redirection reimplementation

--- sources/redirect.c
+++ sources/redirect.c
@@ -6,53 +6,64 @@
 /*   By: qbackaer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 16:53:18 by qbackaer          #+#    #+#             */
-/*   Updated: 2020/02/25 16:54:10 by qbackaer         ###   ########.fr       */
+/*   Updated: 2020/03/09 19:37:15 by qbackaer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/21sh.h"
 
-int	get_file_fd(char *path, char *curr_c)
+static int	get_left_fd(char curr_c)
+{
+	int fd;
+
+	if (curr_c == '>')
+		fd = 1;
+	else if (curr_c == '<')
+		fd = 0;
+	else
+	{
+		fd = (curr_c) - 48;
+		curr_c++;
+	}
+	return (fd);
+}
+
+static int	get_right_fd(char *r_value, char *curr_c)
 {
 	int fd;
 
 	fd = -1;
 	if (*curr_c == '>' && *(curr_c + 1 ) == '\0')
-		fd = open(path, O_WRONLY|O_CREAT, 0666);
+		fd = open(r_value, O_WRONLY|O_CREAT, 0666);
 	else if (*curr_c == '>' && *(curr_c + 1) == '>')
-		fd = open(path, O_WRONLY|O_CREAT|O_APPEND, 0666);
+		fd = open(r_value, O_WRONLY|O_CREAT|O_APPEND, 0666);
 	else if (*curr_c == '<' && *(curr_c + 1) == '\0')
-		fd = open(path, O_RDONLY|O_CREAT, 0666);
+		fd = open(r_value, O_RDONLY|O_CREAT, 0666);
 	if (fd < 0)
 		perror("open error: ");
 	return (fd);
 }
 
-void	apply_redirection(t_tokens *pnode)
+static void	apply_redirection(t_tokens *pnode)
 {
 	int	fd_l;
 	int	fd_r;
 	char	*curr_c;
 
-	curr_c = pnode->string;
-	if (ft_isdigit(*curr_c))
+	if (pnode->next == NULL)
 	{
-		fd_l = (*curr_c) - 48;
-		curr_c++;
+		ft_putendl_fd("redirection error: missing R_VALUE", 2);
+		return ;
 	}
-	else if (*curr_c == '>')
-		fd_l = 1;
-	else if (*curr_c == '<')
-		fd_l = 0;
-	else
-		ft_putendl_fd("redirection format error.", 2);
-	if ((fd_r = get_file_fd(pnode->next->string, curr_c)) < 0)
+	curr_c = pnode->string;
+	fd_l = get_left_fd(*curr_c);
+	if ((fd_r = get_right_fd(pnode->next->string, curr_c)) < 0)
 		return ;
 	dup2(fd_r, fd_l);
 	close(fd_r);
 }
 
-void	redirect(t_tokens *pnode)
+void		redirect(t_tokens *pnode)
 {
 	t_tokens *curr_tok;
 
