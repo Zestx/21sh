6b4f6bc7e0cbe6dfe669f0ddf936bb666aaab803
Quentin Backaert <q.bckrt@gmail.com>
2020-03-10 00:01:21 +0100

    needs more extensive testing, but all redirections except for the here-document are implemented.

--- sources/redirect.c
+++ sources/redirect.c
@@ -12,18 +12,29 @@
 
 #include "../includes/21sh.h"
 
-static int	get_left_fd(char curr_c)
+static int	is_only_digits(char *str)
+{
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+static int	get_left_fd(char **curr_c)
 {
 	int fd;
 
-	if (curr_c == '>')
+	if (**curr_c == '>')
 		fd = 1;
-	else if (curr_c == '<')
+	else if (**curr_c == '<')
 		fd = 0;
 	else
 	{
-		fd = (curr_c) - 48;
-		curr_c++;
+		fd = **curr_c - 48;
+		*curr_c += 1;
 	}
 	return (fd);
 }
@@ -39,8 +50,17 @@ static int	get_right_fd(char *r_value, char *curr_c)
 		fd = open(r_value, O_WRONLY|O_CREAT|O_APPEND, 0666);
 	else if (*curr_c == '<' && *(curr_c + 1) == '\0')
 		fd = open(r_value, O_RDONLY|O_CREAT, 0666);
-	if (fd < 0)
-		perror("open error: ");
+	else if ((*curr_c == '<' || *curr_c == '>') && *(curr_c + 1) == '&')
+	{
+		if (!ft_strcmp(r_value, "-"))
+			return (-2);
+		else if (is_only_digits(r_value))
+			fd = ft_atoi(r_value);
+		else
+			return (-1);	
+	}
+	else
+		return (-1);
 	return (fd);
 }
 
@@ -56,11 +76,13 @@ static void	apply_redirection(t_tokens *pnode)
 		return ;
 	}
 	curr_c = pnode->string;
-	fd_l = get_left_fd(*curr_c);
-	if ((fd_r = get_right_fd(pnode->next->string, curr_c)) < 0)
+	fd_l = get_left_fd(&curr_c);
+	if ((fd_r = get_right_fd(pnode->next->string, curr_c)) == -1)
 		return ;
-	dup2(fd_r, fd_l);
-	close(fd_r);
+	if (fd_r == -2)
+		close(fd_l);
+	else
+		dup2(fd_r, fd_l);
 }
 
 void		redirect(t_tokens *pnode)
@@ -71,9 +93,7 @@ void		redirect(t_tokens *pnode)
 	while (curr_tok)
 	{
 		if (curr_tok->subtype == REDI)
-		{
 			apply_redirection(curr_tok);
-		}
 		curr_tok = curr_tok->next;
 	}
 }
